7f3127a301d72eda50ef109547660dd6db6dc1ed
Lmadushan <lahiruthebest1@gmail.com>
2018-09-16 17:47:30 +0530

    added answer for 01. (c)

--- 2013.c
+++ 2013.c
@@ -1,3 +1,5 @@
+/* 01. (b) */
+/*
 #include <stdio.h>
 
 int power(int x, int n)
@@ -19,4 +21,38 @@ int main()
 
     printf("%d^%d = %d", x, n, result);
     return 0;
+}
+*/
+/* 01. (c) */
+
+#include <stdio.h>
+#include <string.h>
+ 
+void reverse(char [], int, int);
+
+int main()
+{
+    char S[20];
+    int j;
+    int i;
+
+    printf("Enter a string to reverse: ");
+    scanf("%s", S);
+    j = strlen(S);
+    reverse(S, i, j - 1);
+    printf("The string after reversing is: %s\n", S);
+    return 0;
+}
+ 
+void reverse(char S[], int i, int j)
+{
+    char temp;
+    temp = S[i];
+    S[i] = S[j - i];
+    S[j - i] = temp;
+    if (i == j / 2)
+    {
+        return;
+    }
+    reverse(S, i + 1, j);
 }
